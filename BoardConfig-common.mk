13829fca16a3846897e50de7d23f6bfbaf87dd12
l u n a i r e <103412573+Seraphine3@users.noreply.github.com>
2023-08-27 19:43:23 +0800

    b1c1: Boardconfig: Allow Missing Dependencies

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -37,6 +37,9 @@ TARGET_BOARD_COMMON_PATH := device/google/crosshatch/sdm845
 
 BUILD_BROKEN_DUP_RULES := true
 
+# Allow Missing Dependencies 
+ALLOW_MISSING_DEPENDENCIES=true
+
 BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200n8 androidboot.console=ttyMSM0 printk.devkmsg=on
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += ehci-hcd.park=3
